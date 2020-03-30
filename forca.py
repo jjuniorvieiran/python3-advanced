efa66912ea870eded40f27180e5a5479c592d3ef
Junior <junior.jaava@gmail.com>
2020-03-30 19:06:48 -0300

    + creating a file + reading a file + select a random word

--- forca.py
+++ forca.py
@@ -1,10 +1,20 @@
+import random
 
 def jogar():
     print("*********************************")
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip() #to remove \n or space...
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ['_' for letra  in palavra_secreta]
 
     enforcou = False
